1180316138b778c214a70b547b65b329057a779d
Aleja Mendoza <alejamendoza0@gmail.com>
2024-02-16 21:07:20 -0500

    actividad de reconocimiento version 1.0

---
+++ holaMundo/src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -0,0 +1,18 @@
+
+
+package com.mycompany.holamundo;// podemos ver la carpeta donde esta ubicado nuestro archivo y la clase 
+
+
+public class HolaMundo {// esto es una clase llamada Hola Mundo
+                                        /* toda instruccion en java debe estar dentro de una clase
+                                        */
+
+    public static void main(String[] args) {/*aca definimos el metodo main  que es el punto de entrada de todo programa en java 
+                                                                    tambien se puede apreciar que el metodo es estatico , que pertenece a la clase y 
+                                                                    no a las instancias*/
+        System.out.println("Hola Mundo, Funcadion universitaria Compensar!"); /*este es el metodo para impresion por pantalla , 
+                                                                                                                                 se puede apreciar que tambien es una clase 
+                                                                                                                                System es una clase que accede al varios recursos del sistema 
+                                                                                                                                out representa la salida estandar por pantalla*/
+                                                                                                                            }
+}
---
+++ holaMundo2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>//es necesario para usar la funcioncion printf y otras funciones de entrada y salida
+
+int main (){//punto de entrada de cualquier lenguaje en C
+            //nos devuelve un entero que indica que el programa se ejecuto correctamente 
+   
+    printf("hola mundo\n ");//imprime por pantalla la palanra hola mundo, swguido de un salto de linea 
+   
+    return 0; //nos muestra que el programa se ejecuto correctamente
+
+}//es el cierre dek bloque de codigo de la funcion main 
